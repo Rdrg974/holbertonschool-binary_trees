45434bdbdd7612fc4991c97700edf933285b49e7
Arweenn <arwendumont25@outlook.fr>
2024-01-08 03:10:55 -0800

    betty fix

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,14 +5,14 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: void
-*/
+ */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
-    
-    (func)(tree->n);
-    binary_tree_preorder(tree->left, func);
-    binary_tree_preorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+		return;
+
+	(func)(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,14 +5,14 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function call for each node
  * Return: void
-*/
+ */
 
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL || func == NULL)
-        return;
-    
-    binary_tree_inorder(tree->left, func);
-    (func)(tree->n);
-    binary_tree_inorder(tree->right, func);
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	(func)(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
