eb73405d11bdbb4e36c775686a65cb429978e162
Arweenn <arwendumont25@outlook.fr>
2024-01-08 02:05:52 -0800

    betty fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,27 +5,27 @@
  * @parent: parent node
  * @value: value of the node
  * Return: node created
-*/
+ */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
-    
-    new_node = binary_tree_node(NULL, value);
+	if (parent == NULL)
+		return (NULL);
 
-    if (new_node == NULL)
-        return (NULL);
-    
-    if (parent->left != NULL)
-    {
-        parent->left->parent = new_node;
-        new_node->left = parent->left;
-    }
-    parent->left = new_node;
-    new_node->parent = parent;
+	new_node = binary_tree_node(NULL, value);
 
-    return (new_node);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new_node;
+		new_node->left = parent->left;
+	}
+	parent->left = new_node;
+	new_node->parent = parent;
+
+	return (new_node);
 }
