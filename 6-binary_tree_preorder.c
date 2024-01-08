7cc0b4da8acfc544f20e4085485bfd501e9d3c81
Arweenn <arwendumont25@outlook.fr>
2024-01-08 12:18:18 +0100

    description fix

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_preorder - function that goes through
+ *                        a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: void
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - function that goes through a binary tree using in-order traversal
+ * binary_tree_inorder - function that goes through
+ *                       a binary tree using in-order traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function call for each node
  * Return: void
